710cc0123c040d51f5625673ba2f21be9e5f635f
GabrielTheDEV <gabriel4321hh@gmail.com>
2025-06-17 22:10:43 -0300

    Classe menu criado e features adicionados a classe TaskService

--- src/service/TaskService.java
+++ src/service/TaskService.java
@@ -1,4 +1,30 @@
 package service;
 
+import model.Task;
+import java.util.ArrayList;
+import java.util.List;
+
 public class TaskService {
+    private List<Task> task = new ArrayList<>();
+
+    public void addTask(String description) {
+        task.add(new Task(description));
+    }
+    public void getListTask() {
+        if(task.isEmpty()){
+            System.out.println("Nenhuma tarefa adicionada...");
+        }
+        for(int i = 0; i < task.size(); i++){
+            System.out.println(i + ". " + task.get(i));
+        }
+    }
+    public void changeDescription(int index , String newDesc) {
+      //...
+    }
+    public void doneTask() {
+        //...
+    }
+    public void deleteTask() {
+        //...
+    }
 }
