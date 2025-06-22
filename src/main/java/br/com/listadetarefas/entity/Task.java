d8be38ba7fd34fd9014b665a360f5feb83fbf923
GabrielTheDEV <gabriel4321hh@gmail.com>
2025-06-22 16:55:05 -0300

    Projeto e entidade task criado

---
+++ src/main/java/br/com/listadetarefas/entity/Task.java
@@ -0,0 +1,40 @@
+package br.com.listadetarefas.entity;
+
+public class Task {
+    private int id;
+    private String description;
+    private boolean isCompleted;
+
+    public Task() {}
+
+    public Task(String description , boolean isCompleted) {
+        this.description = description;
+        this.isCompleted = isCompleted;
+    }
+    public Task(int id, String description , boolean isCompleted) {
+        this.id = id;
+        this.description = description;
+        this.isCompleted = isCompleted;
+    }
+
+    //Getter e Setter
+
+    public int getId() {
+        return id;
+    }
+    public void setId(int id) {
+        this.id = id;
+    }
+    public String getDescription(){
+        return description;
+    }
+    public void setDescription(String desc){
+        this.description = desc;
+    }
+    public boolean getIsCompleted(){
+        return isCompleted;
+    }
+    public void setIsCompleted(boolean completed){
+        this.isCompleted = completed;
+    }
+}
