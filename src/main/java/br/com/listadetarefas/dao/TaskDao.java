9fba24646f87fdb2573a71a785dbc52a532509f5
GabrielTheDEV <gabriel4321hh@gmail.com>
2025-06-22 17:33:18 -0300

    Classe DatabaseConnection criado

--- src/main/java/br/com/listadetarefas/dao/TaskDao.java
+++ src/main/java/br/com/listadetarefas/dao/TaskDao.java
@@ -1,4 +1,10 @@
 package br.com.listadetarefas.dao;
 
 public class TaskDao {
+
+
+    public void addTask(){}
+    public void getTask(){}
+    public void changeTask(){}
+    public void removeTask(){}
 }
--- src/main/java/br/com/listadetarefas/dao/service/DatabaseConnection.java
+++ src/main/java/br/com/listadetarefas/dao/service/DatabaseConnection.java
@@ -1,4 +1,21 @@
 package br.com.listadetarefas.dao.service;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class DatabaseConnection {
+    private static final String URL = "jdbc:mysql://localhost:3306/x";
+    private static final String USER = "root";
+    private static final String PASSWORD = "toor123";
+
+    public static Connection connect() {
+        try(Connection conn = DriverManager.getConnection(URL, USER, PASSWORD);){
+            System.out.println("Conectado ao banco de dados! ");
+            return conn;
+        }catch(SQLException e){
+            System.out.println("FALHA NA CONEXÃO , ERROR : " + e.getMessage());
+        }
+        return null;
+    }
 }
